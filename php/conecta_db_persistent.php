55f814fc92046de4b8d23abbc3d89aee7cf79da1
ErTreK8 <ericgarciagispert@gmail.com>
2025-01-21 15:58:35 +0100

    Creacion Db Persistent.php

---
+++ php/conecta_db_persistent.php
@@ -0,0 +1,19 @@
+<?php
+$cadena_connexio = 'mysql:dbname=m7redsocial;host=localhost:3335';
+$usuari = 'root';
+$passwd = '';
+try {
+    //Creem una connexió persistent a BDs
+    $db = new PDO($cadena_connexio, $usuari, $passwd,
+        array(PDO::ATTR_PERSISTENT => true));
+
+    if ($db != null) {
+        // echo '<pre>';.
+        // echo "Connexió establerta! \n ";
+        // echo '</pre>';
+        echo 'Connexió establerta!<br>';
+    }
+
+} catch (PDOException $e) {
+    echo 'Error amb la BDs: ' . $e->getMessage() . '<br>';
+}
