aa8069e007c6007c9cff3845ee610ba0bba61ce3
Jesusaez <jesusmiguel.saezv@educem.net>
2025-01-23 15:40:45 +0100

    home

--- web/home.php
+++ web/home.php
@@ -4,9 +4,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home</title>
-        <link rel="stylesheet" href="../css/register.css">
+        <link rel="stylesheet" href="../css/home.css">
     </head>
     <body>
-        
+        <a href="../php/logout.php" class="logout-btn">Logout</a>
     </body>
 </html>
